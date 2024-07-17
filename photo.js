3099daf9f557ffc89061c8ecc3f859c9c23d3114
Jamie <156792628+jamieberridge@users.noreply.github.com>
2024-07-17 17:59:29 +0100

    Update photo.js

--- photo.js
+++ photo.js
@@ -16,22 +16,15 @@ async function getFirstRow() {
         const result = await sql.query('SELECT TOP 1 * FROM dbo.Ingredients');
         const firstRow = result.recordset[0];
         console.log(firstRow['Image']);
+        
     } catch (error) {
         console.error(error);
     } finally {
-        sql.close();
+        await sql.close();
     }
     return firstRow;
 }
 
 let row = getFirstRow();
-let image = row['Image'];
+let imageURL = row['Image'];
 
-// Function to convert Base64 string back to an image and display it
-function base64ToImage(base64String, imageElementId) {
-    const imgElement = document.getElementById(imageElementId);
-    imgElement.src = base64String; 
-}
-
-// Example usage
-base64ToImage(image, "yourImageElementId");  // Replace with your actual image element ID
